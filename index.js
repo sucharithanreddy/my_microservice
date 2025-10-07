5508ce2329ab2a38433588fb91ba207664fb541b
sucharithanreddy <sucharithanreddy@gmail.com>
2025-10-07 11:26:22 +0000

    Adding git credentials to jenkins file.

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => res.send('Hello, DevOps!'));
+app.get('/', (req, res) => res.send('Hello, DevOps World!'));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
